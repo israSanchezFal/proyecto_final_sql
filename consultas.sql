fcafa462d1cc467d1f27fd72ccf52125fc660718
Natalia Agredo <nagredol@itam.mx>
2025-12-02 16:26:21 -0600

    Update consultas N.sql

--- consultas.sql
+++ consultas.sql
@@ -120,3 +120,115 @@ GROUP BY
     1 -- Agrupamos por la primera columna (el CASE)
 ORDER BY 
     banda_velocidad ASC;
+
+
+
+--C5. Calle más peligrosa (Ranking y Contribución)
+WITH rankin_calles AS (
+    SELECT 
+        street_name,
+        COUNT(*) AS total_choques,
+        -- Sumamos todos los conteos para tener el gran total
+        SUM(COUNT(*)) OVER () AS gran_total_choques,
+        -- Ranking para encontrar la #1
+        RANK() OVER (ORDER BY COUNT(*) DESC) AS ranking_peligrosidad
+    FROM crash_location
+    GROUP BY street_name
+)
+SELECT 
+    street_name,
+    total_choques,
+    -- Porcentaje: (Choques en esta calle / Total de choques) * 100
+    ROUND(
+        (total_choques::numeric / NULLIF(gran_total_choques, 0)) * 100, 4
+    ) AS porcentaje_del_total_global
+FROM rankin_calles
+WHERE ranking_peligrosidad = 1;
+
+
+
+    
+--C6. Análisis de 'Hit and Run': Distribución y peso porcentual por tipo de daño
+WITH fugas_por_costo AS (
+    SELECT 
+        damage AS tipo_daño,
+        COUNT(*) AS total_casos,
+        -- Suma total de fugas (sin agrupar) para sacar el porcentaje
+        SUM(COUNT(*)) OVER () AS total_fugas_global
+    FROM crash
+    WHERE hit_and_run = 'TRUE'
+    GROUP BY damage
+)
+SELECT 
+    tipo_daño,
+    total_casos,
+    -- ¿Qué porcentaje representa este daño del total de fugas?
+    ROUND(
+        (total_casos::numeric / NULLIF(total_fugas_global, 0)) * 100, 2
+    ) AS porcentaje
+FROM fugas_por_costo
+ORDER BY total_casos DESC;
+
+
+
+--C7. Mes/Año con mayor cantidad de choques y su porcentaje anual
+WITH datos_por_mes AS (
+    SELECT 
+        EXTRACT(YEAR FROM crash_date) AS anio, 
+        EXTRACT(MONTH FROM crash_date) AS mes, 
+        COUNT(*) AS choques_mes
+    FROM crash
+    GROUP BY anio, mes
+),
+ranking_totales AS (
+    -- Totales anuales y ranking
+    SELECT 
+        anio, 
+        mes, 
+        choques_mes, 
+        SUM(choques_mes) OVER (PARTITION BY anio) AS choques_anio, 
+        -- Ranking para identificar el mes #1 de cada año
+        RANK() OVER (PARTITION BY anio ORDER BY choques_mes DESC) AS ranking_mes
+    FROM datos_por_mes
+)
+SELECT 
+    anio,
+    mes,
+    choques_mes,
+    choques_anio,
+    -- Cálculo del porcentaje (parseando a decimal para precisión)
+    ROUND((choques_mes::numeric / choques_anio) * 100, 2) AS porcentaje_del_anio 
+FROM rankingYtotales
+WHERE ranking_mes = 1
+ORDER BY anio;
+
+
+
+--C8. Horario por zona con mayor cantidad de choques (zonas más peligrosas)--
+WITH zona_y_hora AS (
+    SELECT 
+        crash_location.beat_of_occurrence AS zona,
+        crash_location.street_name AS nombre_calle,
+        crash.crash_hour AS hora,
+        COUNT(*) AS total_choques
+    FROM crash
+    JOIN crash_location ON crash.crash_id = crash_location.crash_id
+    GROUP BY crash_location.beat_of_occurrence, crash_location.street_name, crash.crash_hour
+),
+ranking_horario AS (
+	SELECT 
+        zona, 
+        nombre_calle, 
+        hora, 
+        total_choques, 
+        RANK() OVER (PARTITION BY zona ORDER BY total_choques DESC) AS ranking
+	FROM zona_y_hora
+)
+SELECT 
+    zona, 
+    nombre_calle, 
+    hora AS hora_mas_peligrosa, 
+    total_choques
+FROM ranking_horario
+WHERE ranking = 1 
+ORDER BY zona ASC;
